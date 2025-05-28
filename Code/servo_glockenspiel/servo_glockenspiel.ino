f97c91ecc134d354423c9cf20782ef4225a5719e
ax21078 <shenjiayingadm@outlook.com>
2025-05-28 22:25:04 +0100

    code for three sero

--- Code/servo_glockenspiel/servo_glockenspiel.ino
+++ Code/servo_glockenspiel/servo_glockenspiel.ino
@@ -1,22 +1,36 @@
 #include <Servo.h>
-Servo servo;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
 void setup() {
-  servo.attach(5);
-  servo.write(0); // initial point
   Serial.begin(9600);
+
+  servo1.attach(5);  
+  servo2.attach(7);  
+  servo3.attach(8);  
+
+  // initial at 0
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
+  Serial.println("All servos set to 0°");
+  delay(1000); 
 }
 
 void loop() {
-  if (servo.read() != 0) {
-    Serial.println("Not at origin!");
-  }
-
-  servo.write(90);
-  Serial.println("Moving to 90°");
+  // down to click
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  Serial.println("All servos dropped to 0°");
   delay(300);
 
-  servo.write(0);
-  Serial.println("Returning to 0°");
-  //delay(5000);
+  // up to 0
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
+  Serial.println("All servos returned to 90°");
+  delay(3000);
 }
