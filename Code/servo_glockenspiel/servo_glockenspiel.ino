08bbf8031428c0d87a78e532ab1ac454802590d7
ax21078 <shenjiayingadm@outlook.com>
2025-06-14 15:49:09 +0100

    fuether development

--- Code/servo_glockenspiel/servo_glockenspiel.ino
+++ Code/servo_glockenspiel/servo_glockenspiel.ino
@@ -1,36 +1,68 @@
 #include <Servo.h>
 
-Servo servo1;
-Servo servo2;
-Servo servo3;
+Servo servoC;
+Servo servoD;
+Servo servoE;
+Servo servoF;
+Servo servoG;
+Servo servoA;
+Servo servoB;
+Servo servoC1;
 
 void setup() {
   Serial.begin(9600);
-
-  servo1.attach(5);  
-  servo2.attach(7);  
-  servo3.attach(8);  
+  servoC.attach(0);
+  servoD.attach(1);
+  servoE.attach(2);
+  servoF.attach(3);
+  servoG.attach(4);
+  servoA.attach(5);  
+  servoB.attach(6);  
+  servoC1.attach(7);  
 
   // initial at 0
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(0);
+  servoC.write(0);
+  servoD.write(0);
+  servoE.write(0);
+  servoF.write(0);
+  servoG.write(0);
+  servoA.write(0);
+  servoB.write(0);
+  servoC1.write(0);
   Serial.println("All servos set to 0°");
   delay(1000); 
 }
 
 void loop() {
   // down to click
-  servo1.write(90);
-  servo2.write(90);
-  servo3.write(90);
-  Serial.println("All servos dropped to 0°");
-  delay(300);
+  servoC.write(90);
+  delay(100);
+  servoD.write(90);
+  delay(100);
+  servoE.write(90);
+  delay(100);
+  servoF.write(90);
+  delay(200);
+  servoG.write(90);
+  delay(200);
+  servoA.write(90);
+  delay(200);
+  servoB.write(90);
+  delay(100);
+  servoC1.write(90);
+  delay(100);
+  Serial.println("All servos dropped to 90°");
+  //delay(300);
 
   // up to 0
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(0);
-  Serial.println("All servos returned to 90°");
+  servoC.write(0);
+  servoD.write(0);
+  servoE.write(0);
+  servoF.write(0);
+  servoG.write(0);
+  servoA.write(0);
+  servoB.write(0);
+  servoC1.write(0);
+  Serial.println("All servos returned to 0°");
   delay(3000);
 }
